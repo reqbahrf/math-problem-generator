6d3da7d9197855ca8c229c296a3b346ded55a372
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-01 23:32:15 +0800

    feat: Implement global modal context with useModal hook

---
+++ app/context/useModalContext.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { createContext, useContext } from 'react';
+import useModal from '../hook/useModal'; // Import the useModal hook
+import type { OpenModalProps } from '../types/modal';
+
+interface ModalContextType {
+  openModal: (props: OpenModalProps) => void;
+  closeModal: () => void;
+}
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const ModalProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { modal, openModal, closeModal } = useModal();
+
+  return (
+    <ModalContext.Provider value={{ openModal, closeModal }}>
+      {children}
+      {modal}
+      {/* Render the modal here, it will be controlled by the context */}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModalContext = () => {
+  const context = useContext(ModalContext);
+  if (context === undefined) {
+    throw new Error('useModalContext must be used within a ModalProvider');
+  }
+  return context;
+};
