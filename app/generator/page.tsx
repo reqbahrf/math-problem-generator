0dc80846cb8e961d3427730724ae47cdcf034a44
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-02 13:20:31 +0800

    feat: Implement exit confirmation for math problem generator

--- app/generator/page.tsx
+++ app/generator/page.tsx
@@ -24,14 +24,51 @@ export default function Home() {
     feedback,
     isCorrect,
     error,
+    invalidateCurrentSession,
   } = useMathProblem();
-  const { openModal } = useModalContext();
+  const { openModal, closeModal } = useModalContext();
 
   const renderHistory = useMemo(() => {
     if (problemHistory.length === 0) return;
 
     return problemHistory.map((problem) => <ViewHistoryCard {...problem} />);
   }, [problemHistory]);
+
+  const beforeBack = async (): Promise<void> => {
+    return new Promise((resolve) => {
+      openModal({
+        title: 'Confirm',
+        headerColor: 'bg-red-600 dark:bg-red-400',
+        children: (
+          <div className='flex flex-col gap-2 text-center'>
+            Current session will end. Are you sure you want to leave this page?
+            <div className='flex justify-center gap-2'>
+              <button
+                className='bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded'
+                onClick={() => {
+                  closeModal();
+                  resolve(void 0);
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                className='bg-red-600 dark:bg-red-400 px-4 py-2 rounded'
+                onClick={() => {
+                  invalidateCurrentSession();
+                  resolve(void 0);
+                  closeModal();
+                }}
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        ),
+        size: 'md',
+      });
+    });
+  };
 
   useEffect(() => {
     if (showHistory) {
@@ -54,7 +91,7 @@ export default function Home() {
         </h1>
         {error && <ErrorCard error={error} />}
         <div className='mb-4 flex justify-end'>
-          <BackButton />
+          <BackButton beforeBack={beforeBack} />
         </div>
         <div className='bg-white rounded-lg shadow-lg p-6 mb-6 dark:bg-gray-800'>
           <button
