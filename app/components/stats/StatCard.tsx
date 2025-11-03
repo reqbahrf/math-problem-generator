c462c975543ac8f0b371cc9e2b2b92d28db0e8b1
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-03 13:29:57 +0800

    Feat: Redesign StatCard for current problem session progress

--- app/components/stats/StatCard.tsx
+++ app/components/stats/StatCard.tsx
@@ -1,23 +1,17 @@
 'use client';
-import React, { forwardRef } from 'react';
+import React from 'react';
 import { useMathProblem } from '@/app/context/MathProblemContext';
 
-const StatCard = forwardRef<HTMLButtonElement, {}>(({}, ref) => {
-  const { score, problemHistory, setShowHistory } = useMathProblem();
+const StatCard = () => {
+  const { problem } = useMathProblem();
   return (
-    <div className='bg-white rounded-lg shadow-lg p-6 mb-6 dark:bg-gray-800 flex justify-between items-center'>
-      <div className='text-xl font-bold text-gray-800 dark:text-white justify-self-center'>
-        Score: {score} / {problemHistory.length}
+    <div className='bg-white rounded-lg shadow-lg p-6 mb-6 dark:bg-gray-800 w-full'>
+      <div className='text-md font-bold text-gray-800 dark:text-white text-center'>
+        Correct Answers: {problem?.filter((p) => p.isCorrect).length} /{' '}
+        {problem?.length}
       </div>
-      <button
-        ref={ref}
-        onClick={() => setShowHistory(true)}
-        className='bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 transition'
-      >
-        See History
-      </button>
     </div>
   );
-});
+};
 
 export default StatCard;
