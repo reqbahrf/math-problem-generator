9aaec0502f5ecfadaff99726318820f65a5de89b
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-04 11:40:13 +0800

    feat(ui): Add 'View Summary' button to StatCard upon session completion

--- app/components/stats/StatCard.tsx
+++ app/components/stats/StatCard.tsx
@@ -4,12 +4,23 @@ import { useMathProblem } from '@/app/context/MathProblemContext';
 
 const StatCard = () => {
   const { problem } = useMathProblem();
+  const isProblemsCompletelyAnswered = problem?.every((p) => p.isAnswered);
   return (
-    <div className='bg-white rounded-lg shadow-lg p-6 mb-6 dark:bg-gray-800 w-full'>
+    <div className='bg-white rounded-lg shadow-lg p-6 mb-4 dark:bg-gray-800 w-full'>
       <div className='text-md font-bold text-gray-800 dark:text-white text-center'>
         Correct Answers: {problem?.filter((p) => p.isCorrect).length} /{' '}
         {problem?.length}
       </div>
+      {isProblemsCompletelyAnswered && (
+        <div className='flex justify-center mt-4'>
+          <button
+            type='button'
+            className='bg-blue-600 text-white px-4 py-2 rounded'
+          >
+            View Summary
+          </button>
+        </div>
+      )}
     </div>
   );
 };
