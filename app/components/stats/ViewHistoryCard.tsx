f5ee2d4456237f99f32ccc31f3f2cebfc9fb7160
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-04 11:40:13 +0800

    refactor(ui): Adapt ViewHistoryCard to ProblemSession type and improve readability

--- app/components/stats/ViewHistoryCard.tsx
+++ app/components/stats/ViewHistoryCard.tsx
@@ -1,37 +1,50 @@
 'use client';
-import { ProblemHistory } from '@/lib/@types/problemTypes';
+import { ProblemSession } from '@/lib/@types/problemTypes';
 import React, { memo } from 'react';
 
-const ViewHistoryCard = ({
-  id,
+const ViewHistoryCard: React.FC<ProblemSession> = ({
+  questionId,
   problemText,
   userAnswer,
   isCorrect,
+  problemType,
   solution,
   createdAt,
-}: ProblemHistory) => {
+}) => {
   return (
     <div className='bg-white dark:bg-gray-900 rounded-lg shadow-md p-6 grid grid-cols-[2fr_2fr] gap-4'>
       <div className='md:col-span-1 col-span-2'>
         <h2 className='text-xl text-gray-700 dark:text-gray-300 font-bold mb-2 border-b pb-1'>
-          Problem ID: {id.slice(0, 8)}...
+          Problem ID: {questionId.slice(0, 8)}...
         </h2>
         <p className='text-gray-700 dark:text-gray-300 mb-2'>{problemText}</p>
       </div>
 
       <div className='md:col-span-1 col-span-2'>
         <p className='text-gray-700 dark:text-gray-300 mb-2'>
-          Your Answer: <span className='font-semibold'>{userAnswer || 'not answered'}</span>
+          Your Answer:{' '}
+          <span className='font-semibold'>{userAnswer || 'not answered'}</span>
         </p>
         <p className='text-gray-700 dark:text-gray-300 mb-2'>
           Status:{' '}
           <span
             className={`text-white px-2 py-1 rounded-2xl ${
-              isCorrect === null ? 'bg-gray-600' : (isCorrect ? 'bg-green-600' : 'bg-red-600')
+              isCorrect === null
+                ? 'bg-gray-600'
+                : isCorrect
+                ? 'bg-green-600'
+                : 'bg-red-600'
             }`}
           >
-            {isCorrect === null ? 'unanswered' : (isCorrect ? 'Correct' : 'Incorrect')}
+            {isCorrect === null
+              ? 'unanswered'
+              : isCorrect
+              ? 'Correct'
+              : 'Incorrect'}
           </span>
+        </p>
+        <p className='text-gray-700 dark:text-gray-300 mb-2'>
+          Type: {problemType}
         </p>
         <p className='text-gray-700 dark:text-gray-300 mb-2'>
           Created: {createdAt}
